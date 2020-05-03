01b85eb28da58a5c82ffd1b39fbb8815be2b16ca
Apinya Chabaram <nook1Apinya@gmail.com>
2020-05-03 17:09:01 +0700

    Get Method
    Test get method

--- testFirebase1.py
+++ testFirebase1.py
@@ -1,13 +1,27 @@
 #from .asyncn import process_pool
 from firebase import firebase
 
-firebase=firebase.FirebaseApplication('https://python-firebase-125da.firebaseio.com/',None)
+dat=firebase.FirebaseApplication('https://python-firebase-125da.firebaseio.com/',None)
 data={
     'Age':30,
     'EmpID':'emp02',
-    'Name':'Testing',
+    'Name':'Villa',
     'Position':'Tester'
 
 }
-result=firebase.post('https://python-firebase-125da.firebaseio.com/Employee',data)
+
+
+result=dat.post('https://python-firebase-125da.firebaseio.com/Employee',data)
 print(result)
+
+#fire=firebase.FirebaseApplication('https://python-firebase-125da.firebaseio.com')
+firebase = firebase.FirebaseApplication('https://python-firebase-125da.firebaseio.com/', authentication =None)
+test=firebase.get('/Employee',None)
+print(test)
+
+#test=firebase.get('https://python-firebase-125da.firebaseio.com/Employee',data)
+#test = firebase.get('/Data', None)
+#print(test)
+
+#for i in test:
+#    print(i)
